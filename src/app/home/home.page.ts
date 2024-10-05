b8b22443fe098becd40d9225d07ce48cd498f092
bloschuk <maxim.makki@gmail.com>
2024-10-05 16:58:59 +0300

    создание кнопки

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+interface BookType{
+  book_name: string
+  book_author:string
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  acc: string = "blosch";
+  list_name: string = "My Books"
+  input_name: string = "--"
+  input_author: string = "--"
+  set_value: string = "__"
+  books:BookType[] = []
+
+  set_my_value(){
+    this.books.push({book_name:this.input_name, book_author:this.input_author })
+    this.set_value = this.input_name + " " + this.input_author
+
+  }
   constructor() {}
   
 }
